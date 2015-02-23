d685644c8ab753b9438bbdac14a0d06ee07b41ef
cropleyb <cropleyb@yahoo.com.au>
2015-02-23 12:50:27 +1100

    Finally got my first llt test working...

---
+++ src/length_lookup_table.h
@@ -0,0 +1,15 @@
+#ifndef _length_lookup_table_h
+#define _length_lookup_table_h
+
+typedef unsigned char Colour; // TODO: Move
+typedef unsigned short Mask;
+#define P1 1 // TODO: Constants!
+#define P2 2
+
+template<typename T>
+void processSubstrips(Mask pattern, int firstInd, int lastInd, T *stats, Colour colour)
+{
+
+}
+
+#endif
