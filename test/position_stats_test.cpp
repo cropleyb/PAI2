388c9740efb2339f81270a35d240182746f6f724
cropleyb <cropleyb@yahoo.com.au>
2015-02-25 16:21:07 +1100

    Test inc/dec param

--- test/position_stats_test.cpp
+++ test/position_stats_test.cpp
@@ -2,16 +2,9 @@
 
 #define private public
 
-#include "bdebug.h"
 #include "defines.h"
 #include "loc.h"
 #include "position_stats.h"
-
-// #include <string>
-using std::string;
-//using testing::ElementsAre;
-
-//#define MAX_CANDS 20
 
 class PositionStatsFixture : public testing::Test {
 public:
@@ -28,36 +21,64 @@ using std::vector;
 typedef vector<Loc> LocArr;
 
 TEST_F(PositionStatsFixture, NoStatsYet) {
+	const PriorityLevel &pl = ps.getLengthPriorityLevel(P1, 1);
+	EXPECT_EQ(0, pl.getNumCands());
+
+	Ind found = pl.getCands(locBuffer, countBuffer, 4);
+	EXPECT_EQ(0, found);
+}
+
+TEST_F(PositionStatsFixture, AddSingleP1) {
 	LocArr ll;
 	Loc l1(5,2);
 	ll.push_back(l1);
-	ps.reportLengthCandidates(P1, 1, ll, 1);
+	ps.reportLengthCandidates(P1, 1, ll, 1); // length 1, inc
 
 	const PriorityLevel &pl = ps.getLengthPriorityLevel(P1, 1);
 	EXPECT_EQ(1, pl.getNumCands());
 
-	Ind found = pl.getCands(locBuffer, countBuffer, 4);
+	Ind found = pl.getCands(locBuffer, countBuffer, 4); // Max
 	EXPECT_EQ(1, found);
 	EXPECT_EQ(l1, locBuffer[0]);
 	EXPECT_EQ(1, countBuffer[0]);
-	
-	//ASSERT_THAT(ms.blackCounter, ElementsAre(5,0,0,0,0));
 }
 
-#if 0
-// TODO...
-TEST_F(LengthLookupFixture, CountSingleBlack) {
-	// Do a search of a tree with no possible moves
-	// - should return an invalid Loc instance
-	// MockStats ms;
-	
-	// EXPECT_CALL(ms, reportLengthCandidate(P1, 1, 1, ms.whatever, 1));
-	  // .WillOnce(Return(Loc::INVALID))
-    //MOCK_METHOD5(reportLengthCandidate, void(Colour colour, int length, int numLocs, Loc *locs, int inc));
-	// template<MockStats> 
-	//processSubstrips(&ms); //int firstInd, int lastInd, T *stats, Colour colour)
-	processSubstripsString("    B    ", P1);
-	ASSERT_THAT(ms.blackCounter, ElementsAre(5,0,0,0,0));
-	ASSERT_THAT(ms.whiteCounter, ElementsAre(0,0,0,0,0));
+TEST_F(PositionStatsFixture, AddThreeP2s) {
+	LocArr ll;
+	Loc l1(1,1);
+	Loc l2(2,2);
+	Loc l3(3,3);
+	ll.push_back(l1);
+	ll.push_back(l2);
+	ll.push_back(l3);
+	ps.reportLengthCandidates(P2, 2, ll, 1); // length 1, inc
+
+	const PriorityLevel &pl1 = ps.getLengthPriorityLevel(P1, 1);
+	EXPECT_EQ(0, pl1.getNumCands());
+	const PriorityLevel &pl2 = ps.getLengthPriorityLevel(P2, 2);
+
+	Ind found = pl2.getCands(locBuffer, countBuffer, 4); // Max
+	EXPECT_EQ(3, found);
+	EXPECT_EQ(1, countBuffer[0]);
+	EXPECT_EQ(1, countBuffer[1]);
+	EXPECT_EQ(1, countBuffer[2]);
+
+	// FIXME: remove order dependence
+	EXPECT_EQ(l3, locBuffer[0]);
+	EXPECT_EQ(l2, locBuffer[1]);
+	EXPECT_EQ(l1, locBuffer[2]);
 }
-#endif
+
+TEST_F(PositionStatsFixture, AddAndRemoveSingleP1) {
+	LocArr ll;
+	Loc l1(5,2);
+	ll.push_back(l1);
+	ps.reportLengthCandidates(P1, 1, ll, 1); // length 1, inc
+	ps.reportLengthCandidates(P1, 1, ll, -1); // length 1, dec
+
+	const PriorityLevel &pl = ps.getLengthPriorityLevel(P1, 1);
+	EXPECT_EQ(0, pl.getNumCands());
+
+	Ind found = pl.getCands(locBuffer, countBuffer, 4); // Max
+	EXPECT_EQ(0, found);
+}
