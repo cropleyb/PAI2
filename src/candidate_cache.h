0febc2a64c4c3b427603cd8c31abac7e1e9fc0ce
cropleyb <cropleyb@yahoo.com.au>
2015-02-27 10:28:09 +1100

    CandidateCache now sort of working...

--- src/candidate_cache.h
+++ src/candidate_cache.h
@@ -6,31 +6,30 @@ class CandidateCache
 public:
 	CandidateCache() {}
 
-	Loc getNextMove() {
-		while (_depth >= 0)
+	Loc getNextMove(Depth depth) {
+		Breadth currInd = _currIndex[depth];
+		if (currInd < _moveCount[depth])
 		{
-			if (_currIndex[_depth] < _moveCount[_depth])
-			{
-				return _bigBuffer[_depth][_currIndex[_depth]++];
-			}
-			_depth -= 1;
+			// Move on...
+			_currIndex[depth]++;
+			return _bigBuffer[depth][currInd];
 		}
+		// No more left at current depth
 		return Loc::INVALID;
 	}
 
 	// Get the buffer to fill it up.
     Loc *getBuffer(Depth d) {return _bigBuffer[d]; }
 
-	// Report how many items are used in the buffer.
+	// Report how many items are used in the buffer, and reset the "iterator"
 	void setDepthMoves(Depth d, unsigned char moveCount) { 
 		_moveCount[d] = moveCount;
-		_depth = d;
+		_currIndex[d] = 0;
 	}
 
 private:
 	Loc _bigBuffer[MAX_DEPTH][MAX_CANDS];
 	Breadth _moveCount[MAX_DEPTH];
 	Breadth _currIndex[MAX_DEPTH];
-	Depth _depth;
 };
 
--- test/candidate_cache_test.cpp
+++ test/candidate_cache_test.cpp
@@ -5,9 +5,23 @@
 #include "defines.h"
 #include "loc.h"
 #include "candidate_cache.h"
+#include "bdebug.h"
+
+using testing::ElementsAre;
 
 class CandidateCacheFixture : public testing::Test {
 public:
+	vector<Loc> getMoves(Depth d) {
+		vector<Loc> allMoves;
+		do
+		{
+			Loc l = cc.getNextMove(d);
+			if (!l.isValid()) break;
+			allMoves.push_back(l);
+		}
+		while (true);
+		return allMoves;
+	}
 	CandidateCache cc;
 	Loc locBuffer[MAX_DEPTH][MAX_CANDS];
 };
@@ -16,11 +30,8 @@ using ::testing::AtLeast;
 using ::testing::Return;
 using ::testing::InSequence;
 
-//using std::vector;
-//typedef vector<Loc> LocArr;
-
 TEST_F(CandidateCacheFixture, NoMovesYet) {
-	Loc l = cc.getNextMove();
+	Loc l = cc.getNextMove(0);
 
 	EXPECT_EQ(Loc::INVALID, l);
 }
@@ -30,8 +41,34 @@ TEST_F(CandidateCacheFixture, GetDepth0Buffer) {
     Loc *locs = cc.getBuffer(0);
 	locs[0] = l;
 	cc.setDepthMoves(0,1);
-	Loc l2 = cc.getNextMove();
+	Loc l2 = cc.getNextMove(0);
 
 	EXPECT_EQ(l, l2);
+}
+
+TEST_F(CandidateCacheFixture, GetMovesFrom2DepthLevels) {
+	Loc l1 = Loc(1,1);
+	Loc l2 = Loc(2,2);
+	Loc l3 = Loc(3,3);
+	Loc l4 = Loc(4,4);
+	Loc l5 = Loc(5,5);
+	Loc l6 = Loc(6,6);
+    Loc *locs0 = cc.getBuffer(0);
+	BD(cout << "Before: " << locs0[0]._value << endl);
+	locs0[0] = l1;
+	locs0[1] = l2;
+	locs0[2] = l3;
+	BD(cout << "After: " << locs0[0]._value << endl);
+	cc.setDepthMoves(0,3);
+    Loc *locs1 = cc.getBuffer(1);
+	locs1[0] = l4;
+	locs1[1] = l5;
+	locs1[2] = l6;
+	cc.setDepthMoves(1,3);
+
+	vector<Loc> res_locs1 = getMoves(1);
+	ASSERT_THAT(res_locs1, ElementsAre(l4,l5,l6));
+	vector<Loc> res_locs0 = getMoves(0);
+	ASSERT_THAT(res_locs0, ElementsAre(l1,l2,l3));
 }
 
