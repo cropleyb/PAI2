82d3984ba38aa69b3a332e3c7ed5d3ed6471be1f
cropleyb <cropleyb@yahoo.com.au>
2015-03-01 20:05:01 +1100

    And yet another pass :)

--- test/move_suggester_test.cpp
+++ test/move_suggester_test.cpp
@@ -184,6 +184,40 @@ TEST_F(MoveSuggesterFixture, ExtendOursBeforeTheirs) {
 	EXPECT_EQ(move.isValid(), false);
 }
 
+TEST_F(MoveSuggesterFixture, IterateTwiceNoChange) {
+	PositionStats ps;
+	CandidateCache cc;// TODO: Make this invisible to the user code?
+	MoveSuggester ms(ps, cc);
+
+	Loc l1(1,1);
+	LocArr ll1;
+	ll1.push_back(l1);
+	ps.reportLengthCandidates(P1, 3, ll1, 1);
+
+	Loc l2(2,2);
+	LocArr ll2;
+	ll2.push_back(l2);
+	ps.reportLengthCandidates(P2, 3, ll2, 1);
+
+	Loc move = ms.getNextMove(2);
+	EXPECT_EQ(l1, move);
+
+	move = ms.getNextMove(2);
+	EXPECT_EQ(l2, move);
+
+	move = ms.getNextMove(2);
+	EXPECT_EQ(move.isValid(), false);
+
+	move = ms.getNextMove(2);
+	EXPECT_EQ(l1, move);
+
+	move = ms.getNextMove(2);
+	EXPECT_EQ(l2, move);
+
+	move = ms.getNextMove(2);
+	EXPECT_EQ(move.isValid(), false);
+}
+
 #if 0
     def setUp(self):
         self.pf = PriorityFilter6()
