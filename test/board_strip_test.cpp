ae91da1f598b5c0944f107e67b17e3f0335cbd65
cropleyb <cropleyb@yahoo.com.au>
2015-03-06 12:44:42 +1100

    clean up

--- test/board_strip_test.cpp
+++ test/board_strip_test.cpp
@@ -55,10 +55,26 @@ public:
 		matchRange(occs, leftInd, rightInd, mr);
 	}
 
-	//void expectCandInds(BoardWidth 
+	void expectCandInd(LineTableItem &)
+	{
+	}
+
+	template<typename ... Types>
+	void expectCandInd(LineTableItem &lti, BoardWidth cand, Types ... rest)
+	{
+		lti._candInds.push_back(cand);
+		expectCandInd(lti, rest...);
+	}
+
+	template<typename ... Types>
+	void expectCandInds(LineTableItem &lti, Types ... rest)
+	{
+		lti._candInds.clear();
+		expectCandInd(lti, rest...);
+	}
 
 	MockReporter mr;
-	LineTableItem lti;
+	//LineTableItem lti;
 };
 
 
@@ -67,172 +83,11 @@ TEST_F(BoardStripFixture, NoLocsYet) {
 }
 
 TEST_F(BoardStripFixture, OneMatch) {
+	LineTableItem lti;
 	lti._colour = P1;
 	lti._matchType = Line1;
-	lti._candInds.push_back(1);
-	lti._candInds.push_back(2);
-	lti._candInds.push_back(3);
-	lti._candInds.push_back(4);
+	expectCandInds(lti, 1,2,3,4);
 	EXPECT_CALL(mr, report(lti));
 	processOccString("B       |", 0, 8);
 }
 
-#if 0
-TEST_F(AlphaBetaFixture, NoOptions) {
-	// Do a search of a tree with no possible moves
-	// - should return an invalid Loc instance
-	MockBridge mb;
-	
-	EXPECT_CALL(mb, isOneMove())
-      .WillOnce(Return(true))
-      ;
-	EXPECT_CALL(mb, getOnlyMove())
-      .WillOnce(Return(Loc::INVALID))
-      ;
-
-    AlphaBeta ab(mb);
-
-	Loc move = ab.getBestMove();
-	EXPECT_EQ(move.isValid(), false);
-}
-
-/////////////////////////////////////////////////////
-// Lines
-/////////////////////////////////////////////////////
-
-TEST_F(BoardStripFixture, CountSingleBlack) {
-	LineTableItem *item = processOccString("B    ");
-	EXPECT_EQ(P1, item->_colour);
-	EXPECT_EQ(Line1, item->_matchType);
-	ASSERT_THAT(item->_candInds, ElementsAre(1, 2, 3, 4));
-}
-
-TEST_F(BoardStripFixture, CountOneBlackOtherEnd) {
-	LineTableItem *item = processOccString("    B");
-	EXPECT_EQ(P1, item->_colour);
-	EXPECT_EQ(Line1, item->_matchType);
-	ASSERT_THAT(item->_candInds, ElementsAre(0, 1, 2, 3));
-}
-
-TEST_F(BoardStripFixture, CountOneWhiteOtherEnd) {
-	LineTableItem *item = processOccString("    W");
-	EXPECT_EQ(P2, item->_colour);
-	ASSERT_THAT(item->_candInds, ElementsAre(0, 1, 2, 3));
-}
-
-TEST_F(BoardStripFixture, TwoBlacksAtStart) {
-	LineTableItem *item = processOccString("BB   ");
-	EXPECT_EQ(P1, item->_colour);
-	EXPECT_EQ(Line2, item->_matchType);
-	ASSERT_THAT(item->_candInds, ElementsAre(2, 3, 4));
-}
-
-TEST_F(BoardStripFixture, MixedNoMatch) {
-	LineTableItem *item = processOccString("B   W");
-	EXPECT_EQ(EMPTY, item->_colour);
-	EXPECT_EQ(NoMatch, item->_matchType);
-}
-
-/////////////////////////////////////////////////////
-// Takes
-/////////////////////////////////////////////////////
-
-TEST_F(BoardStripFixture, BlackTakeLeftEmpty) {
-	LineTableItem *item = processOccString("BWW  ");
-	EXPECT_EQ(P1, item->_colour);
-	EXPECT_EQ(Take, item->_matchType);
-	ASSERT_THAT(item->_candInds, ElementsAre(3));
-}
-
-TEST_F(BoardStripFixture, WhiteTakeRightWhite) {
-	LineTableItem *item = processOccString(" BBWW");
-	EXPECT_EQ(P2, item->_colour);
-	EXPECT_EQ(Take, item->_matchType);
-	ASSERT_THAT(item->_candInds, ElementsAre(0));
-}
-
-TEST_F(BoardStripFixture, WhiteTakeRightBlack) {
-	LineTableItem *item = processOccString(" BBWB");
-	EXPECT_EQ(P2, item->_colour);
-	EXPECT_EQ(Take, item->_matchType);
-	ASSERT_THAT(item->_candInds, ElementsAre(0));
-}
-
-TEST_F(BoardStripFixture, WhiteTakeRightEdge) {
-	LineTableItem *item = processOccString(" BBW|");
-	EXPECT_EQ(P2, item->_colour);
-	EXPECT_EQ(Take, item->_matchType);
-	ASSERT_THAT(item->_candInds, ElementsAre(0));
-}
-
-TEST_F(BoardStripFixture, WhiteTakeLeftEdge) {
-	LineTableItem *item = processOccString("WBB |");
-	EXPECT_EQ(P2, item->_colour);
-	EXPECT_EQ(Take, item->_matchType);
-	ASSERT_THAT(item->_candInds, ElementsAre(3));
-}
-
-/////////////////////////////////////////////////////
-// Threats
-/////////////////////////////////////////////////////
-
-TEST_F(BoardStripFixture, BlackThreatEmpty) {
-	LineTableItem *item = processOccString(" WW  ");
-	EXPECT_EQ(P1, item->_colour);
-	EXPECT_EQ(Threat, item->_matchType);
-	ASSERT_THAT(item->_candInds, ElementsAre(0, 3));
-}
-
-TEST_F(BoardStripFixture, WhiteThreatWhite) {
-	LineTableItem *item = processOccString(" BB W");
-	EXPECT_EQ(P2, item->_colour);
-	EXPECT_EQ(Threat, item->_matchType);
-	ASSERT_THAT(item->_candInds, ElementsAre(0, 3));
-}
-
-TEST_F(BoardStripFixture, WhiteThreatBlack) {
-	LineTableItem *item = processOccString(" BB B");
-	EXPECT_EQ(P2, item->_colour);
-	EXPECT_EQ(Threat, item->_matchType);
-	ASSERT_THAT(item->_candInds, ElementsAre(0, 3));
-}
-
-TEST_F(BoardStripFixture, WhiteThreatEdge) {
-	LineTableItem *item = processOccString(" BB |");
-	EXPECT_EQ(P2, item->_colour);
-	EXPECT_EQ(Threat, item->_matchType);
-	ASSERT_THAT(item->_candInds, ElementsAre(0, 3));
-}
-
-/////////////////////////////////////////////////////
-// Blocked
-/////////////////////////////////////////////////////
-
-TEST_F(BoardStripFixture, BlackBlockedRight) {
-	LineTableItem *item = processOccString("BBBBW");
-	EXPECT_EQ(P1, item->_colour);
-	EXPECT_EQ(Blocked, item->_matchType);
-	ASSERT_THAT(item->_candInds, ElementsAre());
-}
-
-TEST_F(BoardStripFixture, BlackBlockedLeft) {
-	LineTableItem *item = processOccString("WBBBB");
-	EXPECT_EQ(P1, item->_colour);
-	EXPECT_EQ(Blocked, item->_matchType);
-	ASSERT_THAT(item->_candInds, ElementsAre());
-}
-
-TEST_F(BoardStripFixture, WhiteBlockedRight) {
-	LineTableItem *item = processOccString("WWWWB");
-	EXPECT_EQ(P2, item->_colour);
-	EXPECT_EQ(Blocked, item->_matchType);
-	ASSERT_THAT(item->_candInds, ElementsAre());
-}
-
-TEST_F(BoardStripFixture, WhiteBlockedLeft) {
-	LineTableItem *item = processOccString("BWWWW");
-	EXPECT_EQ(P2, item->_colour);
-	EXPECT_EQ(Blocked, item->_matchType);
-	ASSERT_THAT(item->_candInds, ElementsAre());
-}
-#endif
