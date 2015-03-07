69b1be7a239322eccbb3efef318bea3b3b8c989c
cropleyb <cropleyb@yahoo.com.au>
2015-03-08 09:18:42 +1100

    .

---
+++ src/line_lookup_table.h
@@ -0,0 +1,22 @@
+#ifndef _line_lookup_table_h
+#define _line_lookup_table_h
+
+#include "defines.h"
+#include "line_pattern.h"
+
+/*
+ * The LineLookupTable maps lengths of 5 to significant pattern types.
+ */
+
+void buildAll();
+
+const int MaxSpanMask = 4 * 4 * 4 * 4 * 4;
+
+// The lengthLookup (and its contained class) is the only
+// public scope variable, and only to LengthLookup.cpp (hopefully :) )
+extern LinePattern lengthLookup[MaxSpanMask];
+// Threats are usually also Line2s - this is a secondary table for the
+// original Line2s.
+extern LinePattern threatLookup[MaxSpanMask];
+
+#endif
