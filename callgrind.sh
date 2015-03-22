3c1206a72b6a5f97b9387f54c2751dd906e1a989
cropleyb <cropleyb@yahoo.com.au>
2015-03-23 10:41:35 +1100

    I'm using "Instruments" now anyway - cg not working?

--- callgrind.sh
+++ callgrind.sh
@@ -1,4 +1,4 @@
 # First, turn on debugging and optimisation in CMakefiles.txt
-~/pubsrc/valgrind-trunk/Inst/bin/valgrind --tool=callgrind debug/tests
+~/pubsrc/valgrind-trunk/Inst/bin/valgrind --tool=callgrind --dsymutil=yes debug/tests
 # Then 
-# ~/pubsrc/valgrind-trunk/Inst/bin/callgrind_annotate callgrind.out.86258 > foo.txt
+# ~/pubsrc/valgrind-trunk/Inst/bin/callgrind_annotate callgrind.out.86258 board_strip.h > foo.txt
