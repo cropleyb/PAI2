a1beca3c1f68c9dc54ece0f789d26b44bc42d123
cropleyb <cropleyb@yahoo.com.au>
2015-04-12 12:22:16 +1000

    Comment common uses

--- valgrind.sh
+++ valgrind.sh
@@ -1,1 +1,8 @@
+# Just errors
 ~/pubsrc/valgrind-trunk/Inst/bin/valgrind ~/Dropbox/pai2/debug/tests
+
+# Leaks
+#~/pubsrc/valgrind-trunk/Inst/bin/valgrind --leak-check=full debug/tests
+
+# Run specific subset of tests (inc leak check)
+#  ~/pubsrc/valgrind-trunk/Inst/bin/valgrind --leak-check=full debug/tests --gtest_filter="*RunAIFixture*"
